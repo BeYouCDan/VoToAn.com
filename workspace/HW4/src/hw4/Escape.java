2853b097717fb3ac1f2c7890a9a3d114420352e5
DANIEL Kim <DANIEL Kim>
2016-09-14 19:25:04 -0700

    workspace removed

--- workspace/HW4/src/hw4/Escape.java
+++
@@ -1,8 +0,0 @@
-package hw4;
-public class Escape{
-   public static void main(String[] args){
-      DarkRoom darkBarnYard = new DarkRoom();
-      darkBarnYard.readFromFile("smallRoom.txt");
-      darkBarnYard.escapeDarkRoom("Stack");
-   }
-}
